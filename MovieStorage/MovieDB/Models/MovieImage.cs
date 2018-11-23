3fab059492cc3ab700abebb88e24f7cb5bf4164f
Jin Kim <wlsgns92@hotmail.com>
2018-11-23 15:03:10 +1300

    recommit

---
+++ MovieStorage/MovieDB/Models/MovieImage.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDB.Models
+{
+    public class MovieImage
+    {
+        public string Title { get; set; }
+        public int Playtime { get; set; }
+        public string Genre { get; set; }
+        public string Rating { get; set; }
+        public string Trailer { get; set; }
+        public IFormFile Image { get; set; }
+    }
+}
---
+++ MovieStorage/MovieDB/Models/SeedData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieDB.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MovieDBContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MovieDBContext>>()))
+            {
+                // Look for any movies.
+                if (context.MovieItem.Count() > 0)
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.MovieItem.AddRange(
+                    new MovieItem
+                    {
+                        Title = "Avengers",
+                        Playtime = 90,
+                        Genre = "Action",
+                        Rating = "Action",
+                        Trailer = "https://www.youtube.com/embed/eOrNdBpGMv8",
+                        Url = "https://i.ytimg.com/vi/Sv_an8I2ME0/maxresdefault.jpg",
+                        Uploaded = "07-10-18 4:20T18:25:43.511Z",
+                        Width = "1280",
+                        Height = "720"
+                    }
+
+
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
